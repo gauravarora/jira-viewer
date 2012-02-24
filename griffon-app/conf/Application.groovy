cad1d53b4574599cd63b9b536a8933fef3685ee8
Gaurav Arora <gauravsworld@gmail.com>
2012-02-25 00:13:02 +0530

    setup

--- griffon-app/conf/Application.groovy
+++ griffon-app/conf/Application.groovy
@@ -1,32 +1,19 @@
 application {
-<<<<<<< HEAD
-    title = 'JiraViewer3'
-    startupGroups = ['jiraViewer3']
-=======
-    title = 'JiraViewer2'
-    startupGroups = ['jiraViewer2']
->>>>>>> afbe12010b23aa2e060f86c199dbb550deb0da55
+	title = 'JiraViewer3'
+	startupGroups = ['jiraViewer3']
 
-    // Should Griffon exit when no Griffon created frames are showing?
-    autoShutdown = true
+	// Should Griffon exit when no Griffon created frames are showing?
+	autoShutdown = true
 
-    // If you want some non-standard application class, apply it here
-    //frameClass = 'javax.swing.JFrame'
-}
-mvcGroups {
-<<<<<<< HEAD
-    // MVC Group for "jira-viewer3"
-    'jiraViewer3' {
-        model      = 'jira.viewer3.JiraViewer3Model'
-        view       = 'jira.viewer3.JiraViewer3View'
-        controller = 'jira.viewer3.JiraViewer3Controller'
-=======
-    // MVC Group for "jira-viewer2"
-    'jira-viewer2' {
-        model      = 'jira.viewer2.JiraViewer2Model'
-        view       = 'jira.viewer2.JiraViewer2View'
-        controller = 'jira.viewer2.JiraViewer2Controller'
->>>>>>> afbe12010b23aa2e060f86c199dbb550deb0da55
-    }
+	// If you want some non-standard application class, apply it here
+	//frameClass = 'javax.swing.JFrame'
+	mvcGroups {
+		// MVC Group for "jira-viewer3"
+		'jiraViewer3' {
+			model      = 'jira.viewer3.JiraViewer3Model'
+			view       = 'jira.viewer3.JiraViewer3View'
+			controller = 'jira.viewer3.JiraViewer3Controller'
+		}
 
+	}
 }
